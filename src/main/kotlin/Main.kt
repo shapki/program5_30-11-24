26241ca77d07f3e930eb105801f850eddaf9a1da
Шапкин Игорь Витальевич <585-22@std.pkgh.edu>
2024-11-30 12:14:29 +0300

    asdas

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,31 @@
+/*Дан массив nums, содержащий n различных чисел в диапазоне [0, n]. Вернуть единственное число в диапазоне, отсутствующее в массиве.*/
+
+import kotlin.system.measureTimeMillis
+
+fun missingNumber(nums: IntArray): Int {
+    val n = nums.size
+    val expectSum = n*(n+1)/2
+    val actualSum = nums.sum()
+    return expectSum - actualSum
+}
+
+fun readInt(): IntArray {
+    return readLine()?.split(Regex("[,\\s]+"))?.map { it.toInt() }?.toIntArray() ?: intArrayOf()
+}
+
+fun main() {
+    print("Массив чисел: ")
+    val nums = readInt()
+
+    testMissingNumber(nums)
+}
+
+fun testMissingNumber(nums: IntArray) {
+    val startTime = System.currentTimeMillis()
+    val result = missingNumber(nums)
+    val endTime = System.currentTimeMillis()
+    val time = endTime - startTime
+    println("Вывод: $result, Время: $time мс")
+
+    println("Пояснение Отсутствующее число в диапазоне [0, ${nums.size}]")
+}
